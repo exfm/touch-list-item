a535ae7593e2bb62f0b44d89db1a5aa36046e2ca
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-09-05 12:30:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# touch-list-item
+
+
+
+
+## Install
+
+
+     npm install touch-list-item
+
+## Testing
+
+    git clone 
+    npm install
+    open test/index.html
---
+++ test/touch-list-item.test.js
@@ -0,0 +1,7 @@
+"use strict";
+
+describe("touch-list-item", function(){
+    it("should have at least one test", function(){
+        assert.fail("Write a test!");
+    });
+});
